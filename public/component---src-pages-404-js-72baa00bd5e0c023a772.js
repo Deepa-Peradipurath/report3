0b98203bda0f41acb607196798667ec7b8783d3e
Deepa P <deepa.P6@cognizant.com>
2020-02-04 21:20:38 +0530

    new public folder

---
+++ public/component---src-pages-404-js-72baa00bd5e0c023a772.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),u=a("/MKj"),o=a("jffE"),r=a("QUlA"),s=a("Q2AE");t.default=function(){return l.a.createElement(u.a,{store:s.a},l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-72baa00bd5e0c023a772.js.map
